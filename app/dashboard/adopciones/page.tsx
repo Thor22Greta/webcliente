894d5eda9fd439d883e8b1e09646a1926b1ad1e4
EMG <edgarmg22@gmail.com>
2025-02-10 17:53:54 +0100

    error en deploy

--- app/dashboard/adopciones/page.tsx
+++ app/dashboard/adopciones/page.tsx
@@ -7,19 +7,18 @@ export const metadata: Metadata = {
   title: 'Adopciones',
 };
 
-export default async function Page(props: { searchParams?: Promise<{ query?: string; page?: string }> }) {
-  const searchParams = await props.searchParams;
-  const query = searchParams?.query || '';
+export default async function Page({ params }: { params: { id: string } }) {
+  const query = params.id; // 
 
   const animales = await fetchFiltradosAnimales(query);
   const usuarios = await fetchFiltradosUsuarios(query);
 
-  const animalesNoAdoptados = animales.filter(animal => !animal.adopted);
-  const animalesAdoptados = animales.filter(animal => animal.adopted);
+  const animalesNoAdoptados = animales.filter((animal) => !animal.adopted);
+  const animalesAdoptados = animales.filter((animal) => animal.adopted);
 
   return (
     <main>
-      <h1 className="lusitana_e85447be-module__j818aG__className text-2xl text-green-600">ADOPCIONES</h1>
+      <h1 className="text-2xl text-green-600">ADOPCIONES</h1>
 
       <AdoptionView usuarios={usuarios} animalesNoAdoptados={animalesNoAdoptados} animalesAdoptados={animalesAdoptados} />
     </main>
