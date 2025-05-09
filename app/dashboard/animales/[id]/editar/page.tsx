eac73a36c0f7b3a8b16fd86840d40efe9786e48b
EMG <edgarmg22@gmail.com>
2025-05-09 11:09:23 +0200

    editado animales 2

--- app/dashboard/animales/[id]/editar/page.tsx
+++ app/dashboard/animales/[id]/editar/page.tsx
@@ -1,12 +1,21 @@
+// app/dashboard/animales/[id]/editar/page.tsx
 import React from 'react';
 import { obtenerAnimalPorId } from '@/app/lib/actions';
 import EditarAnimalForm from '@/app/ui/animales/editar-animal-form';
 import type { Animal } from '@/app/lib/definitions';
 
-export default async function EditarAnimalPage({ params }: { params: { id: string } }) {
-  const row = await obtenerAnimalPorId(params.id);
+type Params = { id: string };
+
+export default async function EditarAnimalPage({ params }: { params: Promise<Params> }) {
+  const { id } = await params;
+  const row = await obtenerAnimalPorId(id);
 
   const animal = row as Animal;
 
-  return <EditarAnimalForm animal={animal} />;
+  return (
+    <main>
+      <h1 className="mb-8 text-xl md:text-2xl text-green-600">Editar Animal</h1>
+      <EditarAnimalForm animal={animal} />
+    </main>
+  );
 }
